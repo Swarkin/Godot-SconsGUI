be1c7f8d98876752b1abf225ed08f7a001a0afac
nanachi <@>
2024-08-31 01:12:40 +0200

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,15 @@ mod app;
 use eframe::egui;
 
 fn main() -> eframe::Result {
-    let native_options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0]),
-        ..Default::default()
-    };
-    eframe::run_native(
-        "eframe template",
-        native_options,
-        Box::new(|cc| Ok(Box::new(app::TemplateApp::new(cc)))),
-    )
+	let native_options = eframe::NativeOptions {
+		viewport: egui::ViewportBuilder::default()
+			.with_inner_size([400.0, 300.0])
+			.with_min_inner_size([300.0, 220.0]),
+		..Default::default()
+	};
+	eframe::run_native(
+		"eframe template",
+		native_options,
+		Box::new(|cc| Ok(Box::new(app::TemplateApp::new(cc)))),
+	)
 }
